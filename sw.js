16614f309013cd4a848bd666157cd64f8f216ae0
majorfrazer <fraser@megadecent.com>
2026-02-15 14:33:44 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='pd62';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['index.html','manifest.json'])).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||e.request.url.includes('script.google.com'))return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(C).then(x=>x.put(e.request,c))}return r}).catch(()=>caches.match(e.request)))});
