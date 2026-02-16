af59a02f8f95cacffc2e4057afa17bbddca00764
majorfrazer <fraser@megadecent.com>
2026-02-17 01:56:29 +1000

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'pd70';
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  if (e.request.url.includes('script.google.com')) return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
